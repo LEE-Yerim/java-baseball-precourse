4fe3080882e2d6b06afc450b756e5aaafedb63f4
이예림 <dulfla_5@naver.com>
2021-06-02 19:33:25 +0900

    feat : 사용자 숫자 변형 기능 추가

--- src/main/java/controller/BaseballGame.java
+++ src/main/java/controller/BaseballGame.java
@@ -2,10 +2,27 @@ package controller;
 
 import view.InputView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballGame {
+    private static final int NUMBER_OF_NUMBERS = 3;
     InputView inputView = new InputView();
 
-    public void start(){
-        String numbers = inputView.inputNumbers();
+    public void start() {
+        List<Integer> userNumbers = createUserNumbers();
+    }
+
+    public List<Integer> createUserNumbers() {
+        String userNumbers = inputView.inputNumbers();
+        return convertUserNumbers(userNumbers);
+    }
+
+    public List<Integer> convertUserNumbers(String numbers) {
+        List<Integer> userNumbers = new ArrayList<>();
+        for (int i = 0; i < NUMBER_OF_NUMBERS; i++) {
+            userNumbers.add(Character.getNumericValue(numbers.charAt(i)));
+        }
+        return userNumbers;
     }
 }
